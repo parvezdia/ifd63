36493beeea6113333bf1e51bef1158f67d36ffc2
Parvez <96809412+parvezdia@users.noreply.github.com>
2022-01-12 10:31:25 +0600

    Add files via upload

--- lesson3c.py
+++ lesson3c.py
@@ -1,2 +1,5 @@
 s="Twinkle, twinkle, little \'star\'\n\tHow I \"wonder\" what you are \n\t\tUp above the world so high \n\t\t\tLike a \\diamond\\ in the sky"
-print(s) 
+
+u=In 1939, thanks to his court case, \"the California Child Actor's\" Bill,\n also known as the “Coogan Act”,\n was passed to protect the rights of child performers and \nentertainers and is still the basis for \ncurrent US legislation.
+print(s)
+print(u)
