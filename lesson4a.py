44c8fb20a9af6c95250ea462b20f010a7a718ded
Parvez <96809412+parvezdia@users.noreply.github.com>
2022-01-19 11:50:11 +0600

    Add files via upload

---
+++ lesson4a.py
@@ -0,0 +1,45 @@
+# x=10
+# x=5
+# x=9
+# print(x)
+
+# x=[2,10,1,13,7]
+# print(x)
+
+# for i in x:
+#     print(i)
+
+# x=[2,10,1,13,7]
+# print(x)
+
+# for i in x:
+#     print(i,end="\t")
+
+fruits=['Mango','Banana','Apple']
+
+print(fruits)
+
+fruits.append('orange')
+print(fruits)
+
+fruits.extend(['jackfruit','grape','pineapple'])
+print(fruits)
+
+fruits.insert(2,"guava")
+print(fruits)
+
+fruits.remove('jackfruit')
+print(fruits)
+
+del fruits[2]
+print(fruits)
+
+print(fruits[-1])
+
+print(fruits[-3])
+
+print(fruits[0:3])
+
+print(fruits[2:6])
+
+print(fruits)
